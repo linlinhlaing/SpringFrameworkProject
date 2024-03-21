458ad2b2b0cb38735c4c37e7765ab26c3b1e9d1b
Lin Lin Hlaing <linlinhlaing@Lins-Air.attlocal.net>
2024-03-21 19:15:35 -0400

    added files for partE-application

---
+++ partE-application/FinalProject-application/src/main/java/application/OrderDAO.java
@@ -0,0 +1,17 @@
+package application;
+
+import framework.Service;
+import framework.Value;
+
+@Service
+public class OrderDAO {
+
+    @Value("smtp.server")
+    private String outgoingMailServer;
+
+    @Value("${hello}")
+    private String hello;
+    public void print() {
+        System.out.println("orderDAO " + hello + " "+outgoingMailServer);
+    }
+}
